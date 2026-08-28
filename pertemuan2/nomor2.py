d1f1ced7eb37120ee2553159439be1975873b41c
CahyoAditya <adityacahyo104@gmail.com>
2026-08-29 00:18:21 +0700

    feat: add answer to pertemuan2/nomor2.py

--- pertemuan2/nomor2.py
+++ pertemuan2/nomor2.py
@@ -22,5 +22,47 @@ menjadi bersih
 Output: Lokasi robot dan update matriks di setiap waktu
 """
 
+# Wait, ini teh tinggal bikin denah tiap waktu dari ubin 3x5?
+# Whatever, saya bikin variabel aja nanti di main
+#
+# Oiya, buat yang pertemuan 2 wajib pakai class nggak sih?
+# Jujur nggak ada perintahnya soalnya...
+
+def print_denah(denah, position, step):
+    print("t =", step)
+    for i in range(len(denah)):
+        for j in range(len(denah[i])):
+            if (i, j) != position:
+                print(denah[i][j], end=" ")
+            else:
+                if denah[i][j] == 1:
+                    denah[i][j] = 0
+                print("x", end=" ")
+            step += 1
+        print()
+    print("***")
+
 if __name__ == "__main__":
-    pass
+    # Jujur baru tau juga kalau bisa langsung bikin denah, nggak kepikiran sebelumnya
+    denah = [
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0],
+        [0, 0, 1, 0, 0]
+    ]
+
+    position = (0, 0)
+    step = 1
+    for i in range(len(denah)):
+        if i % 2 == 0:
+            for j in range(len(denah[i])):
+                position = (i, j)
+                print_denah(denah, position, step)
+                step += 1
+        else:
+            for j in range(len(denah[i]) - 1, -1, -1):
+                position = (i, j)
+                print_denah(denah, position, step)
+                step += 1
+
+    # print("t = 1")
+    # print("x 0 1 0 0\n0 1 0 0 0\n0 0 1 0 0")
